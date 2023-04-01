fff64c59ecead1830e404f3501cb43b683125d8f
SizzlinSeal <liamteale6@gmail.com>
2023-03-31 18:24:48 -0700

    Everything works!

---
+++ README.md
@@ -0,0 +1,13 @@
+# robot.py
+
+This is a simple Python script that simulates a differential drive robot.
+
+The primary purpose of this script is to test new motion algorithms without having to worry about the hardware.
+
+## Dependencies
+
+- graphics.py
+- asyncio
+- time
+- math
+- screeninfo
--- robot.py
+++ robot.py
@@ -1,45 +1,88 @@
 import graphics
-import threading
+import asyncio
 import time
 import math
+import screeninfo
 
-win = graphics.GraphWin("Robot", 500, 500)
+## Get the primary monitor's resolution
+monitor = screeninfo.get_monitors()[0]
+win = graphics.GraphWin("Differential Drive Simulator", monitor.height/2, monitor.height/2)
+win.setCoords(-72, -72, 72, 72)
 
 
+## Clamp a value in a range
+##
+## {number} value - The value to clamp
+## {number} min - The minimum value
+## {number} max - The maximum value
+##
+## returns {number} - The clamped value
+def clamp(value, min, max):
+    if value < min:
+        return min
+    elif value > max:
+        return max
+    else:
+        return value
 
+
+## Robot class
 class Robot:
-    def __init__ (self, trackWidth):
-        self.x = 200
-        self.y = 200
-        self.angularVelocity = 0.01
-        self.lateralVelocity = 1
-        self.heading = 0
-        self.body = graphics.Circle(graphics.Point(self.x, self.y), trackWidth)
+    ## Robot constructor
+    ##
+    ## {number} trackWidth - Robot's track width in inches
+    ## {number} maxSpeed - Robot's maximum speed in inches per second
+    ## {number} x - Robot's initial x position in inches
+    ## {number} y - Robot's initial y position in inches
+    ## {number} theta - Robot's initial angle in radians
+    ##
+    ## returns {Robot} - the robot object
+    def __init__(self, trackWidth, maxSpeed, x = 0, y = 0, theta = 0):
+        self.trackWidth = trackWidth
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.leftSpeed = 0
+        self.rightSpeed = 0
+        self.lastTime = time.time()
+        self.maxSpeed = maxSpeed
+        self.body = graphics.Circle(graphics.Point(self.x, self.y), trackWidth/2)
         self.body.draw(win)
-        self.updateThread = threading.Thread(self.update())
-    def update(self):
+
+    def initialize(self):
+        task = asyncio.create_task(self.updater())
+    
+    ## Update the robot's position
+    async def updater(self):
         while True:
-            dx = self.lateralVelocity * math.cos(self.heading)
-            dy = self.lateralVelocity * math.sin(self.heading)
-            self.x += dx
-            self.y += dy
-            self.lateralVelocity += 0.001
-            self.heading += self.angularVelocity
-            self.body.move(dx, dy)
-            time.sleep(0.01)
-    def move(self, angularVelocity, lateralVelocity):
-        self.angularVelocity = angularVelocity
-        self.lateralVelocity = lateralVelocity
+            # update simulation position
+            deltaTime = time.time() - self.lastTime
+            deltaL = deltaTime * self.leftSpeed
+            deltaR = deltaTime * self.rightSpeed
+            self.lastTime = time.time()
+            ## Calculate the robot's new position
+            ## This is using inverse differential drive kinematics
+            deltaTheta = (deltaR - deltaL) / self.trackWidth
+            if deltaTheta == 0:
+                chord = deltaR
+            else:
+                chord = 2 * math.sin(deltaTheta / 2) * (deltaR / deltaTheta - self.trackWidth / 2)
+            averageTheta = (deltaTheta / 2) + self.theta
+            # update robot position
+            self.x += chord * -math.sin(averageTheta)
+            self.y += chord * math.cos(averageTheta)
+            self.theta += deltaTheta
+            # update robot position on screen
+            self.body.move(self.x - self.body.getCenter().getX(), self.y - self.body.getCenter().getY())
+            print(self.x, self.y, self.theta)
+            # draw the wheels
+            await asyncio.sleep(0.01)
+    
+    ## Set the speed of the robot
+    ##
+    ## {number} leftSpeed - The speed of the left wheel in inches per second
+    ## {number} rightSpeed - The speed of the right wheel in inches per second
+    def setSpeed(self, leftSpeed, rightSpeed):
+        self.leftSpeed = clamp(leftSpeed, -self.maxSpeed, self.maxSpeed)
+        self.rightSpeed = clamp(rightSpeed, -self.maxSpeed, self.maxSpeed)
 
-
-
-
-
-# main function
-def main():
-    robot = Robot(10)
-    input("Press any key to exit ...")
-
-
-if __name__ == "__main__":
-    main()
